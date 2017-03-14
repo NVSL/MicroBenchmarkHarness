12a934807ff40b4a11edbd34a52eb3acd318c1f1
Akshatha Gangadharaiah <agangadh@ucsd.edu>
2017-03-14 11:32:35 -0700

    Update read/write scripts

--- read_test.sh
+++ read_test.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 ## USAGE 
-## bash read_test.sh <number_of_threads> <number of iterations>
-## Default is 16 threads, 5 iterations
+## bash read_test.sh <number of iterations> [random_read]
+## Default is 5 iterations and sequential read.
+## It runs for 16 threads.
 
-n=$1
-
-if [ -z $n ]; then
-   n=16
-fi
-
-k=$2
+k=$1
 
 if [ -z $k ]; then
    k=5
 fi
 
+r=$2
 
+if [ -z $r ]; then
+   r=0
+fi
 
-for t in $(seq 1 $n); do
+for t in {1,2,4,6,8,12,16}; do
     for i in $(seq 1 $k); do
-        ./file_rd.exe tc$t-size2GB-block4KB -tc $t -max $t -r 1
+	if [ $r = 0 ]; then
+	    ./file_rd.exe tc$t-size2GB-block4KB-sequential -tc $t -max $t
+	else
+	    ./file_rd.exe tc$t-size2GB-block4KB-random -tc $t -max $t -r 1	
+	fi
     done;
 done
--- write_test.sh
+++ write_test.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 ## USAGE 
-## bash read_test.sh <number_of_threads> <number of iterations>
+## bash write_test.sh <number of iterations> [block_size]
 ## Default is 16 threads, 5 iterations
 
-n=$1
-
-if [ -z $n ]; then
-   n=16
-fi
-
-k=$2
+k=$1
 
 if [ -z $k ]; then
    k=5
+fi
+
+b=$2
+
+if [ -z $b ]; then
+   b=4096
 fi
 
 
 
 for t in {1,2,4,6,8,12,16}; do
     for i in $(seq 1 $k); do
-        ./file_wr.exe tc$t-size2GB-block4KB -tc $t -max $t
-	./file_wr.exe tc$t-size2GB-block512B -tc $t -max $t -b 512
+	./file_wr.exe tc$t-size2GB-block$b -tc $t -max $t -b $b
     done;
 done
